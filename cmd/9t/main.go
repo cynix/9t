7ded7fb57f4253a960c3f102cd6f8a1328474d53
cynix <cynix@cynix.org>
2019-08-06 23:20:43 +1000

    Fix build

--- cmd/9t/main.go
+++ cmd/9t/main.go
@@ -4,13 +4,14 @@ import (
 	"log"
 	"os"
 
-	ninetail "github.com/admpub/9t"
+	"github.com/admpub/tail"
+	ninetail "github.com/cynix/9t"
 )
 
 func main() {
 	filenames := os.Args[1:]
 
-	runner, err := ninetail.Runner(filenames, ninetail.Config{Colorize: true, ReOpen: true}) // TODO use flags!!
+	runner, err := ninetail.Runner(filenames, ninetail.Config{Config: &tail.Config{ReOpen: true}, Colorize: true}) // TODO use flags!!
 	if err != nil {
 		log.Fatal(err)
 	}
